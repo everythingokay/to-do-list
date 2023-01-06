8618538c71b5b18bdedfe79228fc9504fb7f3185
Alison Feldish <alison.feldish@gmail.com>
2023-01-06 14:00:03 -0500

    create initial components

---
+++ src/NewForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NewForm extends Component {
+    constructor(props) {
+        super(props)
+    };
+
+    render() {
+        return (
+            <div></div>
+        );
+    };
+};
+
+export default NewForm;
---
+++ src/ToDo.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ToDo extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    };
+};
+
+export default ToDo;
