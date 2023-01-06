5f324c9459d37a12bebd53b0b14c759b73fd4870
Alison Feldish <alison.feldish@gmail.com>
2023-01-06 14:35:52 -0500

    finish building form

--- src/NewForm.js
+++ src/NewForm.js
@@ -3,12 +3,35 @@ import React, { Component } from "react";
 class NewForm extends Component {
     constructor(props) {
         super(props);
+        this.state = {task: ""};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    };
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.props.createTask(this.state);
+        this.setState({ task: "" });
     };
 
     render() {
         return (
-            <div>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="task">New Task</label>
+                <input 
+                    type="text" 
+                    placeholder="new task" 
+                    name="task" 
+                    id="task" 
+                    value={this.state.task}
+                    onChange={this.handleChange}
+                />
+                <button>Add</button>
+            </form>
         );
     };
 };
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
 import ToDo from "./ToDo";
+import NewForm from "./NewForm";
 
 class ToDoList extends Component {
     constructor(props) {
         super(props);
         this.state = { todos: [{task: "laundry"}, {task: "dishes"}] };
+        this.create = this.create.bind(this);
     };
+    create(newToDo) {
+        this.setState({
+            todos: [...this.state.todos, newToDo]
+        });
+    };
+    // remove() {
+
+    // };
+
     render() {
         const todos = this.state.todos.map(todo => {
             return <ToDo task={todo.task} />
         });
-
         return (
             <div>
                 <h1>To-Do List</h1>
                 <ul>{todos}</ul>
+                <NewForm createTask={this.create} />
             </div>
         );
     };
